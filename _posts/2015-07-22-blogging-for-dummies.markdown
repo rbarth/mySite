20062d6d4db56e4a5a232ba4e0769f59866ecf68
Rebecca Barth <rbarth5@gmail.com>
2015-07-29 15:28:00 -0400

    fixed footer/background issues

---
+++ _posts/2015-07-22-blogging-for-dummies.markdown
@@ -0,0 +1,14 @@
+---
+layout: post
+title:  "Blogging for Dummies: My First Blog Post"
+---
+Blogging For Dummies
+No, I didn't actually read the book Blogging for Dummies, although I was unsurprised to see that it actually does exist. For all that I enjoy writing, this is the first blog post I have ever written. When I decided that I would add a blogging page to my website, I put off deciding what exactly I was going to blog about. So when I had the paged designed and built and ready for some content, I sat and waited for inspiration to strike.
+
+I spun around in my chair a few times, doodled on a piece of scrap paper, and then went and made a frappe. Ten minutes and a burst of caffeine-induced energy later, I sat back down at my desk and thought about what exactly I wanted out of this blog. Why do people blog? To make their ideas, thoughts, and opinions heard. So what thoughts did I want the world to hear?
+
+As I am just starting out in web design and development, I don't as of yet have any ideas that will revolutionize the industry. I could write a post about why I chose this color scheme for my website, but it would be more like a paragraph than a post. So here it goes. (link to another post here? is that possible? then the below content would be an separate post instead.)
+
+Blogging about my industry: take one
+
+As my personal website, I could choose whatever colors theme I wanted. So I designed something that I thought represented me. How do you start a visual representation of a person? I began with the color scheme. A main color that was neutral but not boring, with highlights and shadows, and a splash of purple because...well it's my favorite color so I just had to work it in there somewhere. So that was me in colors, how else could I represent myself? Rather than using real photographs of places I have been or would like to go, I decided to make the backgrounds of the pages
--- js/portfolio.js
+++ js/portfolio.js
@@ -45,97 +45,127 @@ $(document).ready(function(){
 
 
 
-	$(function() {
-
-    	function abso() {
-        $('#contactMain').css({
-            // position: 'absolute',
-            width: $(window).width(),
-            height: $(window).height()
-        	});
-    	}
-
-    	$(window).resize(function() {
-        abso();
-    	});
-
-    	abso();
-
-	});
-
-	$(function() {
-
-    	function abso() {
-        $('#aboutMain').css({
-            // position: 'absolute',
-            width: $(window).width(),
-            height: $(window).height()
-        	});
-    	}
-
-    	$(window).resize(function() {
-        abso();
-    	});
-
-    	abso();
-
-	});
+	// $(function() {
+	//
+  //   	function abso() {
+  //       $('#contactMain').css({
+  //           // position: 'absolute',
+  //           width: $(window).width(),
+  //           height: $(window).height()
+  //       	});
+  //   	}
+	//
+  //   	$(window).resize(function() {
+  //       abso();
+  //   	});
+	//
+  //   	abso();
+	//
+	// });
 
 	$(function() {
-
-		var wh = $(window).height();
-		var dh = $('#aboutMe').height();
-		if (dh > wh - 100)
+		if ($('main#aboutMain').length > 0)
 		{
-			  $('#aboutMe').css({
-					height: wh - 100
-				});
+				var dh = $("#aboutMe").height();
+				var spacings = 135;
 		}
-		// var ww = $(window).width();
-		//
-		// if (ww < 550)
-		// {
-		//   $('#aboutMe').css({
-		// 		height: wh - 100
-		// 	});
-		// }
-	});
+		else if ($('main#postMain').length > 0)
+		{
+				var dh = $("#postContent").height();
+				var spacings = 140;
+		}
+		else if ($('main#contactMain').length > 0)
+		{
+				var dh = $("#contactContent").height();
+				var spacings = 40;
+		}
 
-	$(function() {
+		else if ($('main#portfolioMain').length > 0)
+		{
+				var dh = $("#portfolioContent").height();
+				var spacings = 325;
+		}
 
-    	function abso() {
-        $('#portfolioMain').css({
-            // position: 'absolute',
-            width: $(window).width(),
-            height: $(window).height()
-        	});
-    	}
+					var wh = $(window).height();
+					var nh = $("#navbar").height();
+					var space = wh - dh - 57 - spacings;
 
-    	$(window).resize(function() {
-        abso();
-    	});
-
-    	abso();
-
-	});
-
-	$(function() {
-
-			function abso() {
-				$('#postMain').css({
-						// position: 'absolute',
-						width: $(window).width(),
-						height: $(window).height()
-					});
-			}
-
-			$(window).resize(function() {
-				abso();
-			});
-
-			abso();
+					if (dh < (wh - nh) && space > 0)
+					{
+					 	$('footer').css('marginTop', space);
+				 	}
+					else {
+						$('footer').css('marginTop', '20');
+					}
 
 	});
+
+
+	// $(function() {
+	//
+  //   	function abso() {
+  //       $('#aboutMain').css({
+  //           // position: 'absolute',
+  //           width: $(window).width(),
+  //           height: $(window).height()
+  //       	});
+  //   	}
+	//
+  //   	$(window).resize(function() {
+  //       abso();
+  //   	});
+	//
+  //   	abso();
+	//
+	// });
+
+	// $(function() {
+	//
+	// 	var wh = $(window).height();
+	// 	var dh = $('#aboutMe').height();
+	// 	if (dh > wh - 100)
+	// 	{
+	// 		  $('#aboutMe').css({
+	// 				height: wh - 100
+	// 			});
+	// 	}
+	// });
+
+	// $(function() {
+	//
+  //   	function abso() {
+  //       $('#portfolioMain').css({
+  //           // position: 'absolute',
+  //           width: $(window).width(),
+  //           height: $(window).height()
+  //       	});
+  //   	}
+	//
+  //   	$(window).resize(function() {
+  //       abso();
+  //   	});
+	//
+  //   	abso();
+	//
+	// });
+	//
+	// $(function() {
+	//
+	// 		function abso() {
+	// 			$('#postMain').css({
+	// 					// position: 'absolute',
+	// 					width: $(window).width(),
+	// 					height: $(window).height()
+	// 				});
+	// 		}
+	//
+	// 		$(window).resize(function() {
+	// 			abso();
+	// 		});
+	//
+	// 		abso();
+	//
+	// });
 
 	// $(function() {
 	//
@@ -153,30 +183,29 @@ $(document).ready(function(){
 	//
 	// });
 
-	$(function() {
-		var dh = $("#formArea").height();
-		var wh = $(window).height();
-		var nh = $("#navbar").height();
-		var lh = $("#contactLine").height();
-		var tp = wh/2 - dh/2 - nh - lh;
-
-		if (tp > 0)
-		{
-			$("#formArea").css({
-			top: tp
-			});
-			// alert("1");
-		}
-
-		else
-		{
-			$("#formArea").css({
-			top: 50
-			});
-			// alert("2");
-		}
-
-	});
+	// $(function() {
+	// 	var dh = $("#formArea").height();
+	// 	var wh = $(window).height();
+	// 	var nh = $("#navbar").height();
+	// 	var lh = $("#contactLine").height();
+	// 	var tp = wh/2 - dh/2 - nh - lh;
+	//
+	// 	if (tp > 0)
+	// 	{
+	// 		$("#formArea").css({
+	// 		top: tp
+	// 		});
+	// 	}
+	//
+	// 	else
+	// 	{
+	// 		$("#formArea").css({
+	// 		top: 50
+	// 		});
+	// 		// alert("2");
+	// 	}
+	//
+	// });
 
 
 
